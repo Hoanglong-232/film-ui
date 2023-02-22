5e6822313354639ea73363dc3c7507a2eadd4637
“Your <you@example.com>
2023-02-22 19:00:24 +0700

    update code

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,6 +6,7 @@ import Card from './Card';
 let API_key = '&api_key=663c2d1726c0c207cde5bd6924dc1dac';
 let base_url = 'https://api.themoviedb.org/3';
 let url = base_url + '/discover/movie?sort_by=popularity.desc' + API_key;
+let arr = ['Popular', 'Horror', 'Kids', 'Drama', 'Comendy'];
 
 const Main = () => {
     const [movieData, setData] = useState([]);
@@ -20,26 +21,48 @@ const Main = () => {
             });
     }, [url_set]);
 
+    const getData = (movieType) => {
+        if (movieType == 'Popular') {
+            url = base_url + '/discover/movie?sort_by=popularity.desc' + API_key;
+        }
+        if (movieType == 'Horror') {
+            url =
+                base_url +
+                '/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc' +
+                API_key;
+        }
+        if (movieType == 'Kids') {
+            url = base_url + '/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc' + API_key;
+        }
+        if (movieType == 'Drama') {
+            url = base_url + '/discover/movie?with_genres=18&primary_release_year=2014' + API_key;
+        }
+        if (movieType == 'Comendy') {
+            url = base_url + '/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc' + API_key;
+        }
+        setUrl(url);
+    };
+
     return (
         <>
             <div className="header">
                 <nav>
                     <ul>
-                        <li>
-                            <a href="#">Popular</a>
-                        </li>
-                        <li>
-                            <a href="#">Theatre</a>
-                        </li>
-                        <li>
-                            <a href="#">Kids</a>
-                        </li>
-                        <li>
-                            <a href="#">Drama</a>
-                        </li>
-                        <li>
-                            <a href="#">Comdie</a>
-                        </li>
+                        {arr.map((value) => {
+                            return (
+                                <li>
+                                    <a
+                                        href="#"
+                                        name={value}
+                                        onClick={(e) => {
+                                            getData(e.target.name);
+                                        }}
+                                    >
+                                        {value}
+                                    </a>
+                                </li>
+                            );
+                        })}
                     </ul>
                 </nav>
                 <form>
