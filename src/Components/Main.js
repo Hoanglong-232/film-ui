832352e438b84e607527de475fd772f104172a34
“Your <you@example.com>
2023-02-22 14:26:38 +0700

    update code

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,25 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import react, { useEffect, useState } from 'react';
 import Card from './Card';
 
+let API_key = '&api_key=663c2d1726c0c207cde5bd6924dc1dac';
+let base_url = 'https://api.themoviedb.org/3';
+let url = base_url + '/discover/movie?sort_by=popularity.desc' + API_key;
+
 const Main = () => {
+    const [movieData, setData] = useState([]);
+    const [url_set, setUrl] = useState(url);
+
+    useEffect(() => {
+        fetch(url_set)
+            .then((res) => res.json())
+            .then((data) => {
+                // console.log(data.results);
+                setData(data.results);
+            });
+    }, [url_set]);
+
     return (
         <>
             <div className="header">
@@ -27,16 +43,24 @@ const Main = () => {
                     </ul>
                 </nav>
                 <form>
-                    <div className="search-btn"></div>
-                    <input type="text" placeholder="Enter Moive Name" className="inputText"></input>
-                    <button>
-                        <FontAwesomeIcon icon={faSearch} />
-                    </button>
+                    <div className="search-btn">
+                        <input type="text" placeholder="Enter Movie Name" className="inputText"></input>
+
+                        <button>
+                            <FontAwesomeIcon icon={faSearch} />
+                        </button>
+                    </div>
                 </form>
             </div>
 
             <div className="container">
-                <Card />
+                {movieData.length == 0 ? (
+                    <p className="notfound">NOT FOUND</p>
+                ) : (
+                    movieData.map((res, pos) => {
+                        return <Card info={res} key={pos} />;
+                    })
+                )}
             </div>
         </>
     );
